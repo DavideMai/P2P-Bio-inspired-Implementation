2f88d2340158ce922acd8778ab09b93d51d45fa9
DavideMai <d.mai@studenti.unibg.it>
2025-08-05 17:57:33 +0200

    feat: added a compile.bat file

---
+++ HelloJade/compile.bat
@@ -0,0 +1,21 @@
+@echo off
+echo ---------------------------------------
+echo Compilazione del progetto JADE in corso
+echo ---------------------------------------
+
+rem === Pulisce la cartella bin (opzionale)
+rmdir /s /q bin
+mkdir bin
+
+rem === Compila tutti i .java in bin/
+javac -cp lib\jade.jar -d bin src\utils\*.java src\temperatureSensors\*.java
+
+if %errorlevel% neq 0 (
+    echo Errore durante la compilazione!
+    pause
+    exit /b %errorlevel%
+) else (
+    echo Compilazione completata con successo.
+)
+
+pause
